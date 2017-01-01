7b83fd22511dfb10022a462d9104988d2a56103d
alin-corodescu <alin.corodescu@gmail.com>
2017-01-01 18:26:26 +0200

    moved Address to shared libs

---
+++ include/Address.h
@@ -0,0 +1,46 @@
+//
+// Created by alin on 12/21/16.
+//
+#pragma once
+#ifndef CLIENT_ADRESS_H
+#define CLIENT_ADRESS_H
+
+#endif //CLIENT_ADRESS_H
+
+class Address
+{
+private:
+    unsigned int privateIP;
+    unsigned short privatePort;
+    unsigned int publicIP;
+    unsigned short publicPort;
+public:
+    void toString();
+
+    unsigned int getPrivateIP() const {
+        return privateIP;
+    }
+
+    unsigned int getPublicIP() const {
+        return publicIP;
+    }
+
+    unsigned short getPublicPort() const {
+        return publicPort;
+    }
+
+    unsigned short getPrivatePort() const {
+        return privatePort;
+    }
+
+
+    Address(unsigned int publicIP,unsigned short publicPort,
+                unsigned int privateIP = 0,unsigned short privatePort = 0);
+
+    /**
+     * constructor for IP:Port format
+     * @param stringAddr
+     * @return
+     */
+    Address(const char* stringAddr);
+};
---
+++ lib/Address.cpp
@@ -0,0 +1,38 @@
+//
+// Created by alin on 12/29/16.
+//
+#include <arpa/inet.h>
+#include <cstring>
+#include <cstdlib>
+#include "../include/Address.h"
+
+Address::Address(unsigned int publicIP,unsigned short publicPort,
+        unsigned int privateIP,unsigned short privatePort)
+{
+    this->publicIP = publicIP;
+    this-> privateIP = privateIP;
+    this->publicPort = publicPort;
+    this->privatePort = privatePort;
+}
+
+Address::Address(const char *stringAddr) {
+    char* buffer;
+    buffer = (char*) malloc(strlen(stringAddr));
+    char * p;
+    p = strtok(buffer,":");
+
+    unsigned int ip = inet_addr(p);
+    publicIP = ntohl(ip);
+
+    p = strtok(NULL,":");
+    unsigned short port = (unsigned short) atoi(p);
+    publicPort = port;
+
+
+
+}
+
+void Address::toString() {
+
+
+}
